572c7a8d186dff98684e2161e54225bf0bedadd5
Kyle <kylekovary@protonmail.com>
2019-02-24 23:15:23 -0800

    bug fixes

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(tidyverse)
 # Read in data
 normalized_data_genelevel_tpm = read_csv("data/normalized_data_genelevel_tpm.csv")
 geneNames = read_csv('data/GeneNames.csv')
-source('code/functions.R')
+source('code/functions.R', local = TRUE)
 
 # Define UI ----
 ui <- fluidPage(
