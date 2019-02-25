ad6f0599cc1f3f23dccd1f6594ef50dfbe050d5f
Kyle <kylekovary@protonmail.com>
2019-02-24 22:01:31 -0800

    removed text that was moved to readme

--- app.R
+++ app.R
@@ -1,9 +1,3 @@
-# sudo nano RNAseq-shiny-20180309-152352-41371.log
-# sudo rm -r RNAseq
-# sudo cp -r Desktop/app_180403.R /srv/shiny-server/apps/RNAseq/app.R
-#  sudo systemctl restart shiny-server
-# sudo su -     -c "R -e \"install.packages(c('ggplot2'), repos='http://cran.rstudio.com/')\""
-
 # Test 123
 library(shiny)
 library(tidyverse)
